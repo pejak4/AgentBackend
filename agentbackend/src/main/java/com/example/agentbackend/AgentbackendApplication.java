56daa70579c0750d4263728f867aef46fcfe1201
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-05-04 19:59:06 +0200

    Initialize maven project

---
+++ agentbackend/src/main/java/com/example/agentbackend/AgentbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.agentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentbackendApplication.class, args);
+	}
+
+}
